1e328ce7fe57b4c91ea8b027d570fa2556778988
SPRITELEMON-LAP\Spritelemon-laptop <spritelemon36@hotmail.com>
2018-01-12 00:49:16 +0700

    update data    api get to matlab

--- controllers/SensorController.js
+++ controllers/SensorController.js
@@ -9,18 +9,28 @@ var schemaController = {};
 function insert_to_db(table, row){
   var schema = db.model(table);
   var data = new schema(row);
-  data.save(function(err, result){
-      if (err){
-        console.log(error);
-      } else {
-        console.log('Pass');
+  schema.findOne({sensID: row.sensID, teamID: row.teamID}).exec(function (err, result) {
+    if (err){
+      console.log(err);
+    } else {
+      if (result == null) {
+        data.save(function(err, result){
+            if (err){
+              console.log(err);
+            } else {
+              console.log('add Data');
+            }
+        });
       }
-  });
+    }
+  })
+
 }
-function find_from_db(table){
+function find_from_db(table, start, stop){
     var schema = db.model(table);
+    var query = { date: { $gte:'2018-01-11 ' + start + ':00', $lte:'2018-01-11 ' + stop + ':00',} }
     return new Promise(resolve => {
-      schema.find({}).exec(function (err, result) {
+      schema.find(query,null,{sort: {sensID: 1}}).exec(function (err, result) {
           if (err){
             console.log(error);
           } else {
@@ -36,64 +46,78 @@ function checkData(data) {
     return 'no data'
   }
 }
-async function mixData() {
+async function mixData(start, stop) {
   let data = {}
-  let temperature = await find_from_db('temperature')
-  let accelerometer = await find_from_db('accelerometer')
-  let din1 = await find_from_db('din1')
+  let temperature = await find_from_db('temperature', start, stop)
+  let accelerometer = await find_from_db('accelerometer', start, stop)
+  let din1 = await find_from_db('din1', start, stop)
   data.temperature = temperature
   data.accelerometer = accelerometer
   data.din1 = din1
   return data
 }
+
 schemaController.save = function(req, res) {
   var teamNumber = [11,12,13,14,15,16,18,19,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,37,38,40,43,44,46,47,48,49,50,52,53,54,60,61]
   // var teamNumber = [5]
   teamNumber.forEach((number) => {
-    console.log(number);
     request('http://10.0.0.10/api/temperature/'+number+'/all', function(error, response, body) {
-      let getdata = JSON.parse(body)
-      if (getdata.statusCode === '00') {
-        getdata.data.forEach((value) => {
-          let data = {
-            teamID: number,
-            sensID: value.sensID,
-            val: value.val,
-            date: value.date
-          }
-          console.log(data)
-          insert_to_db('temperature', data)
-        })
+      console.log('request temperature');
+      if (!body.includes("502 Bad Gateway")) {
+        let getdata = JSON.parse(body)
+        if (getdata.statusCode === '00') {
+          getdata.data.forEach((value) => {
+            let data = {
+              teamID: number,
+              sensID: value.sensID,
+              val: value.val,
+              date: value.date
+            }
+            insert_to_db('temperature', data)
+          })
+        }
+      } else {
+        console.log('temperature Bad Gateway');
       }
     })
     request('http://10.0.0.10/api/accelerometer/'+number+'/all', function(error, response, body) {
-      let getdata = JSON.parse(body)
-      if (getdata.statusCode === '00') {
-        getdata.data.forEach((value) => {
-          var data = {
-            teamID: number,
-            sensID: value.sensID,
-            val_x: value.val_x,
-            val_y: value.val_y,
-            val_z: value.val_z,
-            date: value.date
-          }
-          insert_to_db('accelerometer', data)
-        })
+      console.log('request accelerometer');
+      if (!body.includes("502 Bad Gateway")) {
+        let getdata = JSON.parse(body)
+        if (getdata.statusCode === '00') {
+          getdata.data.forEach((value) => {
+            var data = {
+              teamID: number,
+              sensID: value.sensID,
+              val_x: value.val_x,
+              val_y: value.val_y,
+              val_z: value.val_z,
+              date: value.date
+            }
+            insert_to_db('accelerometer', data)
+          })
+        }
+      } else {
+        console.log('temperature Bad Gateway');
       }
     })
     request('http://10.0.0.10/api/din1/'+number+'/all', function(error, response, body) {
-      let getdata = JSON.parse(body)
-      if (getdata.statusCode === '00') {
-        getdata.data.forEach((value) => {
-          var data = {
-            teamID: number,
-            sensID: value.sensID,
-            val: value.val,
-            date: value.date
-          }
-          insert_to_db('din1', data)
-        })
+      console.log('request din1');
+      if (!body.includes("502 Bad Gateway")) {
+        let getdata = JSON.parse(body)
+        if (getdata.statusCode === '00') {
+          getdata.data.forEach((value) => {
+            var data = {
+              teamID: number,
+              sensID: value.sensID,
+              val: value.val,
+              date: value.date
+            }
+            insert_to_db('din1', data)
+          })
+        }
+      } else {
+        console.log('temperature Bad Gateway');
       }
     })
 
@@ -101,7 +125,9 @@ schemaController.save = function(req, res) {
 }
 
 schemaController.allTeamSensor = function(req, res) {
-  mixData().then(value => {
+  let start = req.params.start.slice(0, 2) + ":" + req.params.start.slice(2)
+  let stop = req.params.stop.slice(0, 2) + ":" + req.params.stop.slice(2)
+  mixData(start, stop).then(value => {
     res.send(value)
   })
 
--- routes/api.js
+++ routes/api.js
@@ -10,13 +10,16 @@ router.get('/', function(req, res) {
 router.get('/demo/:item', function(req, res) {
   res.send(valueDemo[req.params.item])
 });
+router.get('/test', function(req, res) {
+  sensor.test(req, res)
+});
 router.get('/sensor', function(req, res) {
   res.render('../views/sensor/index')
 });
 router.get('/getDataSensor', function(req, res) {
   sensor.save(req, res)
 });
-router.get('/allTeamSensor', function(req, res) {
+router.get('/allTeamSensor/:start/:stop', function(req, res) {
   sensor.allTeamSensor(req, res)
 });
 module.exports = router;
