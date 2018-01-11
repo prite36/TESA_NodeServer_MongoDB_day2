0747449cec45cdb473dcd7f15479d28b2fbb0422
SPRITELEMON-LAP\Spritelemon-laptop <spritelemon36@hotmail.com>
2018-01-12 05:36:51 +0700

    no message

--- routes/api.js
+++ routes/api.js
@@ -2,19 +2,8 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var sensor = require("../controllers/SensorController.js");
+var valueDemo = require("./value.js");
 var http = require('http');
-router.get('/', function(req, res) {
-  res.send('ssss')
-});
-router.get('/demo/:item', function(req, res) {
-  res.send(valueDemo[req.params.item])
-});
-router.get('/test', function(req, res) {
-  sensor.test(req, res)
-});
-router.get('/sensor', function(req, res) {
-  res.render('../views/sensor/index')
-});
 router.get('/getDataSensor', function(req, res) {
   sensor.save(req, res)
 });
